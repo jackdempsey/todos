6f278a6cd82b67c53e34627aff83149e2c77cd4e
Jack Dempsey <jack.dempsey@gmail.com>
2012-08-18 15:14:32 -0700

    added rspec

--- lib/todos/engine.rb
+++ lib/todos/engine.rb
@@ -1,5 +1,10 @@
 module Todos
   class Engine < ::Rails::Engine
+    config.generators do |g|
+      g.test_framework :rspec
+      g.integration_tool :rspec
+    end
+
     isolate_namespace Todos
   end
 end
