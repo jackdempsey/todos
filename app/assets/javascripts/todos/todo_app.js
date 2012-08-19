2d86f07fbe02c5b772b05bdaf85f3ff26fa91ba1
Jack Dempsey <jack.dempsey@gmail.com>
2012-08-18 18:15:55 -0700

    get dashboard showing todomvc app for ember js

---
+++ app/assets/javascripts/todos/todo_app.js
@@ -0,0 +1,8 @@
+//= require todos/todo_app/app
+//= require todos/todo_app/router
+//= require todos/todo_app/models/todo
+//= require todos/todo_app/models/store
+//= require todos/todo_app/controllers/entries
+//= require todos/todo_app/controllers/todos
+//= require todos/todo_app/views/application
+//= require todos/todo_app/views/todos
